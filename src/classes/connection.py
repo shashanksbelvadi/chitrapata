3d199fb2f9b1099be326e92eff1e2a0613de86a9
Shashank Belvadi <sbelvadi@salesforce.com>
2015-04-26 21:03:29 -0700

    create s3 connection and download photo.

---
+++ src/classes/connection.py
@@ -0,0 +1,19 @@
+import boto
+
+
+class Connection(object):
+
+    s3_connection = None
+
+    def __init__(self):
+        self.getconnection()
+
+    def getconnection(self):
+        ''' 
+            Singleton to avoid making duplicate connections 
+            during an execution context.
+        '''
+        if self.connection == None:
+            self.s3_connection = boto.connect_s3()
+
+        return self.s3_connection
