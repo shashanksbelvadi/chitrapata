c529dbda8876bdc7b17f36887ffe124407a8247a
Shashank Belvadi <sbelvadi@salesforce.com>
2015-04-26 23:52:40 -0700

    Adding credentials.py

--- src/classes/connection.py
+++ src/classes/connection.py
@@ -1,12 +1,16 @@
-import boto
+from boto.s3.connection import S3Connection
+import credentials
 
 
 class Connection(object):
+    ACCESS_KEY = ''
+    SECRET_KEY = ''
 
     s3_connection = None
 
-    def __init__(self):
-        pass
+    def __init__(self, access_key, secret_key):
+        self.ACCESS_KEY = credentials.Credentials.ACCESS_KEY,
+        self.SECRET_KEY = credentials.Credentials.SECRET_KEY
 
     def getconnection(self):
         ''' 
@@ -14,6 +18,9 @@ class Connection(object):
             during an execution context.
         '''
         if self.s3_connection == None:
-            self.s3_connection = boto.connect_s3()
+            self.s3_connection = S3Connection(
+                    self.ACCESS_KEY,
+                    self.SECRET_KEY
+            )
 
         return self.s3_connection
---
+++ src/classes/credentials.py
@@ -0,0 +1,4 @@
+class Credentials(object):
+    ACCESS_KEY = 'AKIAJ2WI34RRHHPFAHQQ',
+    SECRET_KEY = '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'
+    
