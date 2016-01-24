3c11769c3a6c71fa9e95316b018860ff3b3bb96f
Shashank Belvadi <sbelvadi@salesforce.com>
2016-01-24 00:36:09 -0800

    Adding methods to retrive authorization information.

--- deye/deye/src/classes/authentication.py
+++ deye/deye/src/classes/authentication.py
@@ -3,8 +3,18 @@ import flickr_api as api
 
 class Authentication(object):
 
+    url = ''
+
     def __init__(self):
         a = api.auth.AuthHandler() #creates the AuthHandler object
         perms = "read" # set the required permissions
-        url = a.get_authorization_url(perms)
-        print url
+        self.url = a.get_authorization_url(perms)
+
+    def get_auth_url(self):
+        return self.url
+
+    def get_oauth_token(self):
+        pass
+
+    def get_oauth_verifier(self):
+        pass
--- deye/deye/src/classes/driver.py
+++ deye/deye/src/classes/driver.py
@@ -3,8 +3,9 @@ import authentication
 
 class Driver(object):
     def __init__(self):
-        c = authentication.Authentication()
-
+        auth = authentication.Authentication()
+        url = auth.get_auth_url()
+        print url
 
 if __name__ == "__main__":
     d = Driver()
